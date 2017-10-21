a13886ee7da081411ab878e030f5ea05d0d21a8a
yidao <1127611018@qq.com>
2017-10-21 13:57:59 +0800

    Update test.js
    modify test.js

--- test/test.js
+++ test/test.js
@@ -1,1 +1,22 @@
-console.log("this is a plugin print");
+function traverseDOM(node){
+  var childLen=node.childNodes.length;
+  if(childLen==0){
+  }else{
+    for(var i=0;i<childLen;i++){
+      var childTmp=node.childNodes[i];
+      if(childTmp.nodeName="DIV"){
+        if(childTmp.style){
+          var obj=window.getComputedStyle(childTmp);
+          if(obj.hasOwnProperty("position")){
+            if(obj["position"]=="fixed"){
+              childTmp.style.display="none";
+            }
+            return;
+          }
+        }
+      }
+      traverseDOM(childTmp);
+    }
+  }
+}
+traverseDOM(document);
