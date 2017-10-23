c2cbb5e87b29127ea3ecc4b2cd293c391c1526ad
yidao <1127611018@qq.com>
2017-10-23 13:55:11 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -5,6 +5,9 @@ function traverseDOM(node){
     for(var i=0;i<childLen;i++){
       var childTmp=node.childNodes[i];
       if(childTmp.nodeType==1){
+        if(childTmp.id=='tanx-popwin-outermm_51073678_5024723_5024723_15460009'){
+          var str="hello world!";
+        }
         var obj=window.getComputedStyle(childTmp);
         if(obj.hasOwnProperty("position")){
           if(obj.position=="fixed"){
