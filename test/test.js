78444a85126b134414cba8227b4a88cacd682568
yidao <1127611018@qq.com>
2017-10-23 21:29:52 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,3 +1,23 @@
+var fixedWithLiResult=false;
+//判断元素position属性值为fixed的元素中是否包含li元素
+function isIncludeLiInFixedEle(node){
+	var childLen=node.childNodes.length;
+	if(childLen==0){
+
+	}else{
+		for(var i=0;i<childLen;i++){
+			var childTmp=node.childNodes[i];
+			if(childTmp.nodeType==1){
+				if(childTmp.nodeName=="LI"){
+					fixedWithLiResult=true;
+					return;
+				}
+			}
+		}
+	}
+}
+
+
 function traverseDOM(node){
   var childLen=node.childNodes.length;
   if(childLen==0){
@@ -11,6 +31,11 @@ function traverseDOM(node){
         var obj=window.getComputedStyle(childTmp);
         if(obj.hasOwnProperty("position")){
           if(obj.position=="fixed" && obj.bottom=="0px"){
+            isIncludeLiInFixedEle(childTmp);
+            if(fixedWithLiResult){
+              fixedWithLiResult=false;
+              return;
+            }
             console.log("the fixed element");
             console.log(childTmp);
             childTmp.style.display="none";
